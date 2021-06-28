4ee5555a034df214ed3d3e391dc41523688b3da4
tomas@tomas <tomasjoao984@gmail>
2021-06-28 19:51:11 +0100

    Terminando a perte do Layout

---
+++ main.js
@@ -0,0 +1,60 @@
+/*function liquidificador(frutas){
+    alert(frutas) Função 
+}
+liquidificador('manga limão')*/
+
+/* Variaveis
+var nome = 'Ana'
+alert(nome)*/ 
+
+/*const nome = 'Amigo' Constantes
+alert(nome)*/
+
+
+/*          Objecto  
+
+const pessoa = {
+    name: 'Tomás',
+    age: 20,
+    falar: function () {
+        alert(pessoa.tomás)        
+    }
+}
+pessoa.falar()*/ 
+
+
+/*Abri e fecha o menu quando clicamos no Icon*/
+
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+console.log(toggle)
+
+
+
+for(const element of toggle){
+    element.addEventListener('click', function(){
+        nav.classList.toggle('show')     })
+}
+
+
+/* Quando clicar em um intem do menu, esconde o menu*/
+
+const links = document.querySelectorAll('nav ul li a')
+for(const link of links) {
+    link.addEventListener('click', function(){
+        nav.classList.remove('show')
+    })
+} 
+/*Muda o header da página quando der scroll*/
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function() {
+    if(window.scrollY >= navHeight){
+        /*Scroll é maior que altura do header*/
+        header.classList.add('scroll')
+    }else{
+        /*Scroll é menor que altura do header*/
+        header.classList.remove('scroll')
+    }
+})
